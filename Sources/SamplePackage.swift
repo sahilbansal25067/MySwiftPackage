48279e19ec4bd05e1782b09c6197409eeadebb2e
Sahil Bansal <sahil.2021@smartenergywater.in>
2023-03-09 02:08:11 +0530

    Initial Commit

---
+++ Sources/SamplePackage.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sahil Bansal on 09/03/23.
+//
+
+import Foundation
+public class SamplePackage {
+    public func hello() {
+        print("My sample package")
+    }
+}
